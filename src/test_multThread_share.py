2f854ec010d1c0dbee7ac6c820f6f3402d233eac
yujianmin <jxyyjm@gmail.com>
2017-06-15 13:53:02 +0800

    Thread share-variables

--- src/test_multThread_share.py
+++ src/test_multThread_share.py
@@ -14,33 +14,62 @@ from multiprocessing import dummy
 
 def test_queue_write(a, lock):
 	with lock:
-		print 'test fun :', a, ctime()
+		print 'test_queue_write :', a, ctime()
 		queue.put(a)
-def test_queue_read(queue, lock):
+def test_queue_read(lock):
 	with lock:
 		while True:
 			if queue.empty() == False:
 				print queue.get(block=True) 
 				##这里的block不管外面的用，只是对queue的get管用##
 			else: break
-
+def test_dict_write(a, lock):
+	with lock:
+		dic[str(a)] = a**2
+		print 'test_dict_write :', a, ctime()
+def test_dict_read(lock):
+	with lock:
+		#for k, v in dic.items():
+		print 'test_dict_read, ', ctime(), dic
+def test_list_write(a, lock):
+	with lock:
+		list.append(a)
+		print 'test_list_write :', a, ctime()
+def test_list_read(lock):
+	with lock:
+		print 'test_list_read, ', ctime(), list
+def test_pipe_write(a, lock):
+	## pipe is used between two ##
+	## queue is used among them ##
+	pass
+def test_pipe_read(lock):
+	pass
 if __name__=='__main__':
 	time_begin  = ctime()
-	Thread_pool = dummy.Pool(4)
+	Thread_pool = dummy.Pool(10)
 	queue       = dummy.Manager().Queue()
+	dic         = dummy.Manager().dict()
+	list        = dummy.Manager().list()
 	print 'current thread  : ', dummy.current_process
 	print 'current process : ', multiprocessing.current_process
 	lock = dummy.Lock()
-	for i in xrange(10):
+	for i in xrange(7):
 		Thread_pool.apply_async(test_queue_write, args=(i, lock))
-		Thread_pool.apply_async(test_queue_read,  args=(queue, lock))
+		Thread_pool.apply_async(test_queue_read,  args=(lock, ))
+		Thread_pool.apply_async(test_dict_write,  args=(i, lock))
+		Thread_pool.apply_async(test_dict_read,   args=(lock,))
+		Thread_pool.apply_async(test_list_write,  args=(i, lock))
+		Thread_pool.apply_async(test_list_read,   args=(lock,))
 	Thread_pool.close()
 	Thread_pool.join()
 	print 'queue as following:(here queue had been empty, because queue.get above)'
-	test_queue_read(queue, lock)
-	print 'current thread  : ', dummy.current_process
+	test_queue_read(lock)
+	print 'dict as following:'
+	test_dict_read(lock)
 	time_end = ctime()
+	print 'current thread  : ', dummy.current_process
 	print '== last =='
 	print 'time begin :', time_begin
 	print 'time end   :', time_end
+	print 'Manager 的各个变量，可以在进程之间相互共享'
 
