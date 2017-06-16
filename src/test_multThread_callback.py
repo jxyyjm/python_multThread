46dbb13a170b5a23a2a54bfaed2f49c94c46b55e
yujianmin <jxyyjm@gmail.com>
2017-06-16 09:13:34 +0800

    callback

--- src/test_multThread_callback.py
+++ src/test_multThread_callback.py
@@ -5,16 +5,14 @@ import multiprocessing
 from time import ctime, sleep
 from multiprocessing import dummy
 '''
-	callback only exit when async 
+	notice:
+	1) callback only exit when async ;
+	2) callback only input one arg, but this arg can be anyType , what a happy life;
 '''
 
 def test_fun(a, lock):
 	with lock:
 		b = a
-		a += a
-		a = a/2
-		a = a%2
-		a = a+1
 		a = a**2
 		print 'test fun :', b, ctime(), 'res :', a
 		return a
@@ -24,6 +22,10 @@ def print_test_res(a):
 	## callback 函数默认接收前面func的返回值   ##
 	with lock:
 		print 'callback get input is : ', a
+def test_fun2(a, lock):
+	with lock:
+		print 'test fun :', a, ctime()
+		return {str(a): str(a**2)}
 
 if __name__=='__main__':
 	time_begin  = ctime()
@@ -32,7 +34,8 @@ if __name__=='__main__':
 	result      = []
 	for i in xrange(10):
 		#Thread_pool.apply_async(test_fun, args=(i, lock), callback=print_test_res)
-		Thread_pool.apply_async(test_fun, args=(i, lock), callback=result.append)
+		#Thread_pool.apply_async(test_fun, args=(i, lock), callback=result.append)
+		Thread_pool.apply_async(test_fun2, args=(i, lock), callback=print_test_res)
 	Thread_pool.close()
 	Thread_pool.join()
 	time_end = ctime()
